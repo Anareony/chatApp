3e0fb592684e0c2e9f2e85b1e6edf02434a6e750
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-04-24 18:22:35 +0700

    fix: delete shared ui

--- frontend/src/shared/UiButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, ButtonProps } from "@mui/material";
-
-interface CustomButtonProps extends ButtonProps {}
-
-export function UiButton({ ...props }: CustomButtonProps) {
-  return <Button {...props}></Button>;
-}
--- frontend/src/shared/UiInput.tsx
+++
@@ -1,9 +0,0 @@
-import { TextField, TextFieldProps } from "@mui/material";
-
-export function UiInput({ ...props }: TextFieldProps) {
-  return (
-    <div>
-      <TextField {...props} />
-    </div>
-  );
-}
