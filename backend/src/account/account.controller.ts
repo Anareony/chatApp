70f88b04c53ab16b8d03dc416ff778d0ecb0f74b
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-17 22:30:26 +0700

    feat(backend): loading avatars

--- backend/src/account/account.controller.ts
+++ backend/src/account/account.controller.ts
@@ -1,13 +1,48 @@
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseFilePipeBuilder,
+  Patch,
+  Post,
+  Request,
+  Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiBody, ApiConsumes, ApiOkResponse } from '@nestjs/swagger';
 import { AccountDto, PatchAccountDto } from './dto';
 import { AccountService } from './account.service';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { SessionInfo } from 'src/auth/session-info.decorator';
 import { getSessionInfoDto } from 'src/auth/dto';
+import path = require('path');
+import { v4 as uuidv4 } from 'uuid';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { join } from 'path';
+import { of } from 'rxjs';
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/profileimages',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+    },
+  }),
+};
+
+const MAX_PROFILE_PICTURE_SIZE_IN_BYTES = 5 * 1024 * 1024;
 
 @Controller('account')
-@UseGuards(AuthGuard)
+@UseGuards(JwtAuthGuard)
 export class AccountController {
   constructor(private AccountService: AccountService) {}
 
@@ -28,5 +63,41 @@ export class AccountController {
     @SessionInfo() session: getSessionInfoDto,
   ) {
     return this.AccountService.patchAccount(session.id, body);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', storage))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  uploadFile(
+    @SessionInfo() session: getSessionInfoDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({ fileType: 'image/jpeg' })
+        .addMaxSizeValidator({ maxSize: MAX_PROFILE_PICTURE_SIZE_IN_BYTES })
+        .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
+    )
+    file,
+  ) {
+    return this.AccountService.patchAccount(session.id, {
+      avatarUrl: file.filename,
+    });
+  }
+
+  @Get('profile-image/:imagename')
+  findProfileImage(@Param('imagename') imagename, @Res() res) {
+    return of(
+      res.sendFile(join(process.cwd(), 'uploads/profileimages/' + imagename)),
+    );
   }
 }
