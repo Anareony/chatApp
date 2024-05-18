d124d5a7ab4b498a0e18b643da5f5cd1f67f6957
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-18 14:26:58 +0700

    fix(backend): minor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AccountModule } from './account/account.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [DbModule, AuthModule, UsersModule, AccountModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, AuthModule, UsersModule, AccountModule, ChatModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
