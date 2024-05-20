f05261b7cd22a6d934dbe6f5b5fe769e9d146096
Chudoyakov Roman <chudoyakov.roman@gmail.com>
2024-05-20 23:55:42 +0700

    refactor(frontend): change api

--- client/src/app/providers/redux-provider.tsx
+++ client/src/app/providers/redux-provider.tsx
@@ -1,26 +1,20 @@
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
-import { signUpApi } from "@/features/auth/signup/model";
-import { signInApi } from "@/features/auth/signin/model";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { signOutApi } from "@/features/auth/signout/model";
-import { getAccountApi } from "@/pages/home";
-import { patchAccountApi } from "@/pages/edit";
+import { patchAccountApi } from "@/features/edit-info/model";
+import { getAccountApi } from "@/entities/account";
+import { authApi } from "@/shared/api";
 
 export const store = configureStore({
   reducer: {
-    [signUpApi.reducerPath]: signUpApi.reducer,
-    [signInApi.reducerPath]: signInApi.reducer,
-    [signOutApi.reducerPath]: signOutApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
     [patchAccountApi.reducerPath]: patchAccountApi.reducer,
     [getAccountApi.reducerPath]: getAccountApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({}).concat([
-      signUpApi.middleware,
-      signInApi.middleware,
-      signOutApi.middleware,
+      authApi.middleware,
       patchAccountApi.middleware,
       getAccountApi.middleware,
     ]),
